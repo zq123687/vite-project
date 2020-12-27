e8db844004fcfd9d69736490dccc05b3735b5dd4
zhangqi <2049956761@qq.com>
2020-12-28 01:12:31 +0800

    Initialize project

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <button @click="count++">count is: {{ count }}</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue'
+
+export default {
+  name: 'Home',
+  setup () {
+    const count = ref(0)
+
+    return {
+      count
+    }
+  }
+}
+</script>
